3baa96ec8b8f507003f367d4e6999fbd1fff20eb
JAYNO <JAYNO@MacRentui-MacBookPro-2.local>
2021-09-13 12:36:54 +0900

    0913

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+var btn = document.getElementById('submit');
+
 app.get('/', (req,res) => res.send('흠 연결은 됐구먼')) 
 app.listen(port, () => console.log('Listening is ${port} !'))
+
+// btn.addEventListener('click', function(){ 
+//   btn
+//  });
